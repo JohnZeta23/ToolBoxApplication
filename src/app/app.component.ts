1588c372c0ba31a5943e77f7ebebb1b498516631
JohnZeta23 <johndelacruz638@gmail.com>
2023-03-07 13:03:38 -0400

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Home', url: '/home', icon: 'home' },
+    { title: 'Genero', url: '/genero', icon: 'male-female' },
+    { title: 'Edad', url: '/edad', icon: 'calendar' },
+    { title: 'Universidad', url: '/universidades', icon: 'school' },
+    { title: 'Clima', url: '/clima', icon: 'thunderstorm' },
+    { title: 'Acerca De', url: '/acercade', icon: 'help-circle' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+
   constructor() {}
 }
